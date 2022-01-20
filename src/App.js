a3980852ce21606aead6157aba045ccb5ecc41de
Carlos Diaz <carlosdiaz@Carloss-MacBook-Pro.local>
2022-01-19 19:11:58 -0500

    In this commit I add the css and some funtionalities to the dashboard.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import HomeBar from './components/HomeBar';
+import EventsList from './components/EventsList'
 
 function App() {
+  
   return (
     <div className="App">
+      <HomeBar />
+      <EventsList />
     </div>
   );
 }
---
+++ src/components/Event.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Events({name}) {
+    return (
+        <div className='events'>
+            <h1>{name}</h1>;
+        </div>
+    )
+}
+
+export default Events
