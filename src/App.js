2bb5af976af34794083ad25afb92e509b9df959f
Holly <holly@shop-grok.com>
2022-01-20 11:11:55 +1100

    update to new firebase setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    // Main router that allows routes to connect to the app.
+    <BrowserRouter>
+      <div className="App">
+        {/* Only one route to show up at a time */}
+        <Routes>
+          <Route path="/dashboard"/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCqAeCjOYaqU9zOOiAJII0jsNN2s0F55b4",
-    authDomain: "eventprism-10a0d.firebaseapp.com",
-    projectId: "eventprism-10a0d",
-    storageBucket: "eventprism-10a0d.appspot.com",
-    messagingSenderId: "770286239071",
-    appId: "1:770286239071:web:380bdbe8df631a9887caea",
-    measurementId: "G-FDF8GK6HGM"
+    apiKey: "AIzaSyB65r8zyqLvmKFja-23cmHXs1MlaJ43hmQ",
+    authDomain: "eventprism-d0a40.firebaseapp.com",
+    projectId: "eventprism-d0a40",
+    storageBucket: "eventprism-d0a40.appspot.com",
+    messagingSenderId: "29217795495",
+    appId: "1:29217795495:web:65c6120b7a69b3dee01a82",
+    measurementId: "G-PQDTG9ZWY1"
 };
 
 // Initialize Firebase
