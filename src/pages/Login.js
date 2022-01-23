b0c8420a7144248441b9cc8359454575e2517195
Holly <holly@shop-grok.com>
2022-01-23 12:17:35 +1100

    start to implement login functionality using email and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,27 @@
 import React, { useRef } from 'react';
 import Signin from '../components/Login'
+import { signInWithEmailAndPassword }from 'firebase/auth'
+import { auth } from '../utils/firebase'
 
 function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
+    
+
+    const login = async ()=>{
+        try{
+            await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+            .then(()=>{
+                if(user){
+                    window.location = "/dashboard"
+                }
+
+            })
+        }
+        catch(error){
+            alert(error.message)
+        }
+    }
     return <div>
       <Signin
         title = "Login"
