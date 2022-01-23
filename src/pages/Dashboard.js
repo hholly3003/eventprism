d5ab13cd2404521d0cfc744a41a873d22fd87ecd
Holly <holly@shop-grok.com>
2022-01-23 13:03:22 +1100

    progress on dashboard pages

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function Dashboard() {
+  const [user,setUser] = useState({})
+  
+  //getting the object only of the active user or user who is signed in
+  useEffect(()=>{
+    auth.onAuthStateChanged((currentUser)=>{
+      if(currentUser.uid){
+          setUser(currentUser.uid);
+          
+      }
+    })
+  })
   return (
         <div>
             THIS IS Dashboard
