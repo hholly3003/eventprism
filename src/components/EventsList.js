5c8c8e346123d2a57fedafc333fbe77d3bccacb1
Carlos Diaz <carlosdiaz@Carloss-MacBook-Pro.local>
2022-01-23 13:12:53 -0500

    In this commit I added more information for display, and andded a search bar functionality by type of events.

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,31 +1,51 @@
 import React from 'react'
-import Events from './Events'
-import {useState, useEffect, Fragment} from 'react'
+import Events from './Event'
+import {useState, useEffect, Fragment, useRef} from 'react'
 import {v1} from 'uuid';
 import UserInput from './UserInput';
+import axios from 'axios';
 
 
 function EventsList({name}) {
     const [items, setItems] = useState([]);
-    const [type, setType] = useState('events');
+    const [type, setType] = useState('');
+    const typeRef = useRef()
+    const client_id = 'MjUzNzU2NTF8MTY0MjI5NTUyNi40ODgzNzgz'
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     useEffect(async () => {
-        await fetch(`https://api.seatgeek.com/2/events?&venue.state=NY&client_id=MjUzNzU2NTF8MTY0MjI5NTUyNi40ODgzNzgz&per_page=100&format=json`)
-        .then((response) => response.json())
-        .then((json) => {
-        const {events} = json
-        setItems(events)
+        const getEvent = async () => {
+            try {
+                const response = await axios.get(`https://api.seatgeek.com/2/events?type=${type}&geoip=true&client_id=${client_id}&per_page=100&format=json`)
+                const {events} = response.data;
+                
+                setItems(events);
+
+            } catch(error){
+                console.log(error);
+            }
         }
-        );
+        return getEvent()
     }, [type]);
     return (
         <div className='events-list'>
-            <UserInput />
+            <UserInput 
+                setType={setType}
+                typeRef={typeRef}
+            />
             <div className='container'>
             {
                 items.map((event) => {
+                    const {stats} = event;
                     return <Fragment key={v1()}>
-                            <Events name={event.venue.name}/>
+                            <Events 
+                            title={event.short_title} 
+                            image = {event.performers[0].image} 
+                            date={event.datetime_local.split('T').shift().split('-')} 
+                            months={months}
+                            score={event.score}
+                            lowest_price={stats.lowest_price}
+                            />
                         </Fragment>
                 })
             }
@@ -36,3 +56,11 @@ function EventsList({name}) {
 }
 
 export default EventsList
+
+// await fetch(`https://api.seatgeek.com/2/events?&venue.state=NY&client_id=MjUzNzU2NTF8MTY0MjI5NTUyNi40ODgzNzgz&per_page=100&format=json`)
+//         .then((response) => response.json())
+//         .then((json) => {
+//         const {events} = json
+//         setItems(events)
+//         }
+//         );
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
-function UserInput() {
+function UserInput({setType, typeRef}) {
   return (
   <>
-  <div className="header">
-                <h4>devfinder</h4>
-                <div className="darkMode" >
-                    <p id="modeText">DARK</p>
-                    <img src="./assets/icon-moon.svg" id = "dark_btn"alt=""/>
-                </div>  
-            </div>       
-          <div className="search" id="search_container">
-            <img src="./assets/icon-search.svg" alt=""/>
-            <input id ="input"  type="text" placeholder="Search Github username..."/>
-            <div className="search_btn">
-              <button id="search">Search</button>
-            </div>
-          </div>
-
-          <div id="error">
-            <p>User not found</p>
-          </div>
+    <div className="header">
+    </div>       
+    <div className="search" id="search_container">
+      <select>
+        <option></option>
+      </select>
+      <input ref={typeRef} id ="input" type="text" placeholder="Search Events by Type"/>
+      <div className="search_btn">
+        <button id="search" onClick={() => setType(typeRef.current.value)}>Search</button>
+      </div>
+    </div>
   
   </>
   )
