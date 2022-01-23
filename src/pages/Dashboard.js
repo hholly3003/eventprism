d09141bd64f039cda9c25e5e82b849e785a72548
Holly <holly@shop-grok.com>
2022-01-23 13:01:38 +1100

    progress on Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { auth } from '../utils/firebase';
+
 
 function Dashboard() {
+  const [user,setUser] = useState({})
+  
+  //getting the object only of the active user or user who is signed in
+  useEffect(()=>{
+    auth.onAuthStateChanged((currentUser)=>{
+      if(currentUser.uid){
+          setUser(currentUser.uid);
+          
+      }
+    })
+  })
   return (
         <div>
             THIS IS Dashboard
