24cf1b05895358ed0ded6b878208a631713f78f1
Holly <holly@shop-grok.com>
2022-01-25 11:10:29 +1100

    fix merge conflict

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,28 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import "../styles/UserInput.css";
-// import { doc, setDoc } from "firebase/firestore";
-// import db from "../utils/firebase";
-
-function UserInput({ setType, typeRef }) {
-  return (
-    <>
-      <div className="search" id="search_container">
-        <select>
-          <option></option>
-        </select>
-        <input
-          ref={typeRef}
-          id="input"
-          type="text"
-          placeholder="Search Events by Type"
-        />
-        <div className="search_btn">
-          <button id="search" onClick={() => setType(typeRef.current.value)}>
-            Search
-          </button>
-        </div>
-=======
 import '../styles/UserInput.css'
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -57,7 +33,6 @@ function UserInput({setType, typeRef, setSearchType, searchType}) {
       <input ref={typeRef} id ="input" type="text" placeholder="Search Events by Type"/>
       <div className="search_btn">
         <button id="search" onClick={() => setType(typeRef.current.value)}>Search</button>
->>>>>>> ffe6be4b4b65e95b8d3faf6fd18eadf26c134062
       </div>
     </>
   );
