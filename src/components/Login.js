65e90e28f957b9e62606b0a53acbdf60c4f81164
Holly <holly@shop-grok.com>
2022-01-31 03:11:42 +1100

    incorporate logo to login and logout, added overlay to login container

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from'../images/image.png'
 import '../styles/Login.css';
 
 function Login({title, button, href, link, headerStatement, emailInput, passwordInput, btnFunction}) {
   return(
     <div className='login'>
         <div className='login-container'>
+            <img src={logo} id="logo"/>
             <h1 className='login-heading'>{title}</h1>
             <input ref={emailInput} className='login-email' type="email" placeholder='Email'/>
             <input ref={passwordInput} className='login-password' type="password" placeholder='Password'/>
