5f46e06da54e1ba6a047ff5df693d65aca741a81
Carlos Diaz <carlosdiaz@Carloss-MacBook-Pro.local>
2022-02-01 11:07:49 -0500

    In this commit I fixed the front end for the landing page. Now it resize with the browser.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,6 +2,8 @@ import React from "react";
 import EventsList from "../components/EventsList";
 import HomeBar from "../components/Homebar";
 import '../styles/LandingPageDesign.css';
+// import peopleImg from '../images/Landing-Image.png'
+import logoLanding from '../images/image.png'
 
 function Homepage() {
   function redirectLogin() {
@@ -9,14 +11,18 @@ function Homepage() {
   }
   return (
     <div className="homepage-container">
-      <div id="landingBGDesign">
-        <img src="./Landing-Image.png" alt="Silhouette Crowd People" />
-      </div>
-      <div id="logo">
-        <img id="logo-image" src='./unnamed.png' alt="EventPrism Logo" />
-      </div>
-      <div id="login-bttn">
-        <button id='fade-in-fade-out' onClick={redirectLogin}>Click here to Enter</button>
+      <div className="sub-container-landing">
+        <div className="logo-btn-conatainer">
+          <div className="logo-container-landing">
+            <img id="logo-image-landing" src={logoLanding} alt="EventPrism Logo" />
+          </div>
+          <div className="login-btn-landing">
+            <button id='fade-in-fade-out' onClick={redirectLogin}>Click here to Enter</button>
+          </div>
+        </div>
+        <div className="landingBGDesign">
+          {/* <img src={peopleImg} alt="Silhouette Crowd People" id="people-img"/> */}
+        </div>
       </div>
     </div>
   );
